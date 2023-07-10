a7c01901eedfe87b060b13ad9ab331f3ae3c204e
Jamie Lawrence <jamie@ideasasylum.com>
2023-07-10 22:00:52 +0100

    Remove the association counter
    Don't even remember what this was

--- lib/counter.rb
+++ lib/counter.rb
@@ -3,7 +3,6 @@ require "counter/engine"
 require "counter/railtie"
 require "counter/integration/counters"
 require "counter/integration/countable"
-require "counter/integration/association_counter"
 
 module Counter
   # Your code goes here...
