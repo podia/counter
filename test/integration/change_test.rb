b7033c0615bb1a0f8052a2474e92bc97fe01fd4a
Jamie Lawrence <jamie@ideasasylum.com>
2023-07-12 23:34:44 +0100

    More tests

--- test/integration/change_test.rb
+++ test/integration/change_test.rb
@@ -6,44 +6,49 @@ class ChangesTest < ActiveSupport::TestCase
     product = Product.create! user: user, price: 1000
     product = product.reload
     assert !product.has_changed?(:price)
+    assert !product.has_changed?(:price, from: 1000)
+    assert !product.has_changed?(:price, to: 1000)
+    product.update! price: 1001
+    assert !product.has_changed?(:price, to: ->(price) { price < 1000 })
+    assert !product.has_changed?(:price, from: ->(price) { price > 1000 })
   end
 
-  test "from ANY to ANY" do
+  test "changed from ANY to ANY" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
     assert product.has_changed?(:price)
   end
 
-  test "from ANY to value" do
+  test "changed from ANY to value" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
     assert product.has_changed?(:price, to: 2000)
   end
 
-  test "from value to ANY" do
+  test "changed from value to ANY" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
     assert product.has_changed?(:price, from: 1000)
   end
 
-  test "from block to ANY" do
+  test "changed from block to ANY" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
     assert product.has_changed?(:price, from: ->(p) { p < 2000 })
   end
 
-  test "from ANY to block" do
+  test "changed from ANY to block" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
     assert product.has_changed?(:price, to: ->(p) { p > 1000 })
   end
 
-  test "from block to block" do
+  test "changed from block to block" do
     user = User.create!
     product = Product.create! user: user, price: 1000
     product.update! price: 2000
@@ -59,5 +64,13 @@ class ChangesTest < ActiveSupport::TestCase
     assert !product.has_changed?(:price,
       from: ->(p) { p < 2000 },
       to: ->(p) { p > 1000 })
+  end
+
+  test "unchanged from value to value" do
+    user = User.create!
+    product = Product.create! user: user, price: 1000
+    product.update! price: 1000
+    assert !product.has_changed?(:price, from: 1000)
+    assert !product.has_changed?(:price, to: 1000)
   end
 end
