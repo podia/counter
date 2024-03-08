5bc713cc9af5c73997460344b978ffa1676d78e2
Mario Zugaj <mario@podia.com>
2024-03-08 18:28:54 +0100

    Try something new

--- app/models/concerns/counter/recalculatable.rb
+++ app/models/concerns/counter/recalculatable.rb
@@ -7,19 +7,12 @@ module Counter::Recalculatable
     elsif definition.manual?
       raise Counter::Error.new("Can't recalculate a manual counter")
     else
-      with_lock do
-        new_value = definition.sum? ? sum_by_sql : count_by_sql
-
-        result = self.class.upsert(
-          attributes.without("id", "created_at", "updated_at").symbolize_keys.merge(value: new_value),
-          unique_by: [:parent_type, :parent_id, :name],
-          on_duplicate: Arel.sql("value = counter_values.value + EXCLUDED.value"),
-          record_timestamps: true
-        )
-
-        self.class.find_by(result.to_a.first)
-      end
+      update!(value: recalc_value)
     end
+  rescue ActiveRecord::RecordNotUnique
+    self.class
+      .find_by(parent_type:, parent_id:, name:)
+      .update!(value: recalc_value)
   end
 
   def count_by_sql
@@ -33,5 +26,11 @@ module Counter::Recalculatable
   # use this scope when recalculating the value
   def recalc_scope
     parent.association(definition.association_name).scope
+  end
+
+  private
+
+  def recalc_value
+    definition.sum? ? sum_by_sql : count_by_sql
   end
 end
